fef7539b71dbd1e65cc605312752eee6045c5a64
Cha <ckwntjr128@naver.com>
2022-07-07 01:38:42 +0900

    join

--- jpa/jpql/JpaMain.java
+++ jpa/jpql/JpaMain.java
@@ -12,21 +12,20 @@ public class JpaMain {
         EntityTransaction tx = em.getTransaction();
         tx.begin();
         try {
-            Member member = new Member();
-            member.setUsername("c");
-            member.setAge(10);
-            em.persist(member);
-//
-//            Member singleResult = em.createQuery("select m from Member as m where m.username = :username", Member.class)
-//                    .setParameter("username", "c")
-//                    .getSingleResult();
 
-            List<MemberDTO> resultList = em.createQuery("select new jpql.MemberDTO(m.username, m.age) from Member as m", MemberDTO.class).getResultList();
+            String parm_query = "select m from Member as m where m.username = :username"; // 나중에 :username에 인자값 전달
+            String DTO_query = "select new jpql.MemberDTO(m.username, m.age) from Member as m"; /// 추가로 class는 DTO 클래스로
+            String join_query = "select m from Member as m join m.team as t";
+            String left_query = "select m from Member as m left join Team as t order by m.id desc"; // id로 내림차순
+            String seta_query = "select m from Member m , Team t where m.username = t.name";
+            String query = "select m from Member as m left join Team as t on t.name = 'A'"; // 외래키 조인 x, on 으로 조인인
+            List<Member> result = em.createQuery(query, Member.class)
+                    .getResultList();
 
-            System.out.println("username :  " + resultList.get(0).getUsername());
+//            Member m = result.get(result.size()-1);
+//            System.out.println("member name : "+ m.getTeam());
 
-
-            tx.commit(); // insert 쿼리를 transaction commit 순간 발생
+            tx.commit(); // insert 쿼리를 transaction commit 순간 발생D
         }catch (Exception e){
             tx.rollback();
         }finally {
