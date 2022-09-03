32997bc596b44650c5206af78931453b30335abb
Cha <ckwntjr128@naver.com>
2022-09-03 21:04:23 +0900

    respond

---
+++ spring_mvc/servlet/Response.java
@@ -0,0 +1,44 @@
+package hello.servlet.basic.response;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "responseHeaderServlet", urlPatterns = "/response-header")
+public class ResponseHeaderServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // status-line
+        response.setStatus(HttpServletResponse.SC_OK);
+
+        //response-headers
+        response.setHeader("Content-Type", "text/plain; charset=utf-8");
+        response.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+        response.setHeader("Pragma", "no-cache");
+        response.setHeader("my-header", "hello");
+
+        redirect(response);
+
+        PrintWriter writer= response.getWriter();
+        writer.println("okay");
+    }
+
+    private void cookie(HttpServletResponse response){
+        Cookie cookie = new Cookie("myCookie", "good");
+        cookie.setMaxAge(600);
+        response.addCookie(cookie);
+    }
+
+    private void redirect(HttpServletResponse response) throws IOException{
+
+        response.setStatus(302);
+        response.setHeader("Location", "/basic/hello-form.html");
+        // -> response.sendRedirect("/basic/hello-form.html");
+    }
+}
